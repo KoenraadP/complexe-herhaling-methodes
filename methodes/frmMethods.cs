9ca196c3a753d88b2cdc30f57c9a114ffcdfe982
koenraad.pecceu <koenraad.pecceu@gmail.com>
2025-02-11 12:00:23 +0100

    Finish solution

--- methodes/frmMethods.cs
+++ methodes/frmMethods.cs
@@ -19,15 +19,14 @@ namespace methodes
         private void Add1()
         {
             // cijfers ophalen uit de textboxes + opslaan in double variabelen
-            
-
+            double nr1 = Convert.ToDouble(txtNumber1.Text);
+            double nr2 = Convert.ToDouble(txtNumber2.Text);
 
             // optellen en resultaat opslaan in aparte variabele
-            
+            double sum = nr1 + nr2;
 
             // resultaat in onderste textbox plaatsen
-            
-
+            txtResult.Text = sum.ToString();
         }
 
         // Add1 methode wordt hieronder gebruikt bij de eerste button
@@ -38,8 +37,7 @@ namespace methodes
             // alle code zit al in de methode zelf
             // deze methode kan je niet overzetten naar een andere app tenzij je in die andere app
             // exact hetzelfde soort formulier met dezelfde namen opbouwt
-            
-
+            Add1();
         }
 
         // methode 2: void methode met parameters
@@ -47,11 +45,10 @@ namespace methodes
         private void Add2(double d1, double d2)
         {
             // optellen en resultaat opslaan in aparte variabele
-            
+            double sum = d1 + d2;
 
             // resultaat in onderste textbox plaatsen
-            
-
+            txtResult.Text = sum.ToString();
         }
 
         // Add2 methode wordt hieronder gebruikt bij de tweede button
@@ -59,25 +56,29 @@ namespace methodes
         private void btnAdd2_Click(object sender, EventArgs e)
         {
             // deze keer moet ik de parameters invullen tussen haakjes want ik heb parameters voorzien bij de methode
-            // ik moet wel binnen de haakjes ook de conversie doen want ik heb gezegd dat ik als parameters 'decimal' waarden gebruik
+            // ik moet wel binnen de haakjes ook de conversie doen want ik heb gezegd dat ik als parameters 'double' waarden gebruik
             // deze methode kan al iets makkelijker overgezet worden, enkel de txtResult kan voor problemen zorgen in de methode
-            
 
+            //double d1 = Convert.ToDouble(txtNumber1.Text);
+            //double d2 = Convert.ToDouble(txtNumber2.Text);
+            //Add2(d1, d2);
+
+            Add2(Convert.ToDouble(txtNumber1.Text),
+                Convert.ToDouble(txtNumber2.Text));
         }
 
         // methode 3: return methode zonder parameters
         private double Add3()
         {
             // cijfers ophalen uit de textboxes + opslaan in double variabelen
-
-
+            double d1 = Convert.ToDouble(txtNumber1.Text);
+            double d2 = Convert.ToDouble(txtNumber2.Text);
 
             // optellen en resultaat opslaan
-
+            double sum = d1 + d2;
 
             // resultaat terug sturen uit de methode als 'uitkomst' van de methode (return)
-            return 0;
-
+            return sum;
         }
 
         // Add3 methode wordt hieronder gebruikt bij de derde button
@@ -87,18 +88,21 @@ namespace methodes
             // hier moet ik enkel nog vermelden wat er moet gebeuren met de 'uitkomst' van de methode
             // moet ook omgezet worden naar string want de uitkomst van de methode is een decimal
             // opnieuw minder bruikbaar om over te zetten naar een andere app omdat de nieuwe app dan ook een txtNumber1 en txtNumber2 nodig heeft
-            
 
+            //double sum = Add3();
+            //txtResult.Text = sum.ToString();
+
+            txtResult.Text = Add3().ToString();            
         }
 
         // methode 4: return methode met parameters
         private double Add4(double d1, double d2)
         {
             // optellen en resultaat opslaan in variabele
-
+            double sum = d1 + d2;
 
             // resultaat terug sturen uit de methode als 'uitkomst' van de methode (return)
-            return 0;
+            return sum;
         }
 
         // Add4 methode wordt hieronder gebruikt bij de vierde button
@@ -111,7 +115,10 @@ namespace methodes
             // 3) uiteindelijk ook weer naar string omzetten om in de textbox te krijgen
             // deze methode is echter het meest makkelijk te hergebruiken in een andere app omdat er in de methode zelf nog geen namen van controls (textboxes, buttons, ...) staan
             
-
+            double d1 = Convert.ToDouble(txtNumber1.Text);
+            double d2 = Convert.ToDouble(txtNumber2.Text);
+            double sum = Add4(d1, d2);
+            txtResult.Text = sum.ToString();
         }
 
     }
